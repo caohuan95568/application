59045debce6ea3677fac821d0b813e00020fff4d
caohuan95568 <602269452@qq.com>
2018-07-16 01:00:48 +0800

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public JsonResult returnJson(string name,string value)
+        {
+            bool isAdmin = false;
+            //TODO: Check the user if it is admin or normal user, (true-Admin, false- Normal user)  
+            string output = isAdmin ? "Welcome to the Admin User" : "Welcome to the User";
+
+            return Json(output, JsonRequestBehavior.AllowGet);  
+        }
+    }
+}
